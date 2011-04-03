efa056dc9984b1edc9efe34384e182f18737fe79
delano <delano@solutious.com>
2011-04-03 16:17:45 -0400

    Release 1.1

--- lib/net/ssh/multi/version.rb
+++ lib/net/ssh/multi/version.rb
@@ -7,10 +7,10 @@ module Net; module SSH; module Multi
     MAJOR = 1
 
     # The minor component of the library's version
-    MINOR = 0
+    MINOR = 1
 
     # The tiny component of the library's version
-    TINY  = 1
+    TINY  = 0
 
     # The library's version as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
